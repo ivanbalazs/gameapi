c02587b0e883482be922283765f9bba58fe47f84
Balazs Ivan <balazs.ivan@kasa.com>
2023-05-27 02:22:23 +0200

    Connect4 - Check win state + tests

--- src/connect4/connect4.spec.ts
+++ src/connect4/connect4.spec.ts
@@ -1,24 +1,33 @@
 import { Connect4, Status } from './';
 
+let game;
+
 describe('Connect4', () => {
+  beforeEach(() => {
+    game = new Connect4();
+  });
+
   test('field is properly generated', () => {
-    const game = new Connect4({ columns: 3, rows: 2 });
+    game = new Connect4({ columns: 3, rows: 2 });
     expect(game.status.field).toHaveLength(3);
     expect(game.status.field?.[0]).toEqual([]);
   });
 
-  test('drop token in column fills column properly', () => {
-    const game = new Connect4();
+  test('drop token in column fills column and players are turned properly', () => {
+    expect(game.status.currentPlayer).toBe(1);
     game.drop(1);
+    expect(game.status.currentPlayer).toBe(2);
     game.drop(1);
+    expect(game.status.currentPlayer).toBe(1);
     game.drop(1);
+    expect(game.status.currentPlayer).toBe(2);
     game.drop(2);
     expect(game.status.field?.[1]).toEqual([1, 2, 1]);
     expect(game.status.field?.[2]).toEqual([2]);
   });
 
   test('cannot drop in filled column', () => {
-    const game = new Connect4({ columns: 1, rows: 3 });
+    game = new Connect4({ columns: 1, rows: 3 });
     game.drop(0);
     game.drop(0);
     game.drop(0);
@@ -28,11 +37,7 @@ describe('Connect4', () => {
   });
 
   test('game is a draw', () => {
-    const game = new Connect4({ columns: 2, rows: 3 });
-    const dropAndTurn = (column: number, status = Status.TURN) => {
-      game.drop(column);
-      expect(game.status.status).toBe(status);
-    };
+    game = new Connect4({ columns: 2, rows: 3 });
     dropAndTurn(0);
     dropAndTurn(0);
     dropAndTurn(0);
@@ -40,4 +45,68 @@ describe('Connect4', () => {
     dropAndTurn(1);
     dropAndTurn(1, Status.DRAW);
   });
+
+  test('win by connecting a column', () => {
+    dropAndTurn(0);
+    dropAndTurn(1);
+    dropAndTurn(0);
+    dropAndTurn(1);
+    dropAndTurn(0);
+    dropAndTurn(1);
+    dropAndTurn(2);
+    dropAndTurn(1, Status.WIN);
+  });
+
+  test('win by connecting a row', () => {
+    dropAndTurn(0);
+    dropAndTurn(1);
+    dropAndTurn(2);
+    dropAndTurn(3);
+    dropAndTurn(4);
+    dropAndTurn(0);
+    dropAndTurn(1);
+    dropAndTurn(5);
+    dropAndTurn(2);
+    dropAndTurn(5);
+    dropAndTurn(4);
+    dropAndTurn(5);
+    dropAndTurn(3, Status.WIN);
+  });
+
+  test('win by an ascending diagonal', () => {
+    dropAndTurn(0);
+    dropAndTurn(1);
+    dropAndTurn(2);
+    dropAndTurn(3);
+    dropAndTurn(4);
+    dropAndTurn(5);
+    dropAndTurn(1);
+    dropAndTurn(3);
+    dropAndTurn(2);
+    dropAndTurn(3);
+    dropAndTurn(3);
+    dropAndTurn(4);
+    dropAndTurn(2, Status.WIN);
+  });
+
+  test('win by a descending diagonal', () => {
+    dropAndTurn(0);
+    dropAndTurn(1);
+    dropAndTurn(2);
+    dropAndTurn(3);
+    dropAndTurn(4);
+    dropAndTurn(5);
+    dropAndTurn(3);
+    dropAndTurn(2);
+    dropAndTurn(1);
+    dropAndTurn(1);
+    dropAndTurn(1);
+    dropAndTurn(6);
+    dropAndTurn(2, Status.WIN);
+  });
 });
+
+function dropAndTurn(column: number, status = Status.TURN) {
+  game.drop(column);
+  expect(game.status.status).toBe(status);
+}
--- src/connect4/index.ts
+++ src/connect4/index.ts
@@ -3,14 +3,23 @@ import { Base } from '../base';
 export enum Status {
   TURN = 'turn',
   DRAW = 'draw',
-  WON = 'won',
+  WIN = 'win',
 }
+
+interface checkLineProps {
+  start: number;
+  inverted?: boolean;
+  fixed?: boolean;
+}
+
+const CONNECT = 4;
 
 export class Connect4 extends Base {
   #columns: number;
   #rows: number;
   #field?: number[][];
   #status = Status.TURN;
+  #current = { row: 0, col: 0 };
 
   constructor({ columns, rows }: { columns: number; rows: number } = { columns: 7, rows: 6 }) {
     super({ minPlayers: 2, maxPlayers: 2 });
@@ -28,7 +37,7 @@ export class Connect4 extends Base {
   }
 
   generateField() {
-    this.#field = getFilledArr(this.#columns).map((_, column) => []);
+    this.#field = getFilledArr(this.#columns).map(() => []);
   }
 
   drop(column: number) {
@@ -38,11 +47,12 @@ export class Connect4 extends Base {
     if (this.#field[column].length === this.#rows) {
       throw new Error('This column is full');
     }
-    this.#field[column].push(this.currentPlayer);
-    if (this.#isDraw()) {
+    const count = this.#field[column].push(this.currentPlayer);
+    this.#current = { col: column, row: count - 1 };
+    if (this.#isWin()) {
+      this.#status = Status.WIN;
+    } else if (this.#isDraw()) {
       this.#status = Status.DRAW;
-    } else if (this.#hasWon()) {
-      this.#status = Status.WON;
     } else {
       this.nextPlayer();
     }
@@ -53,8 +63,44 @@ export class Connect4 extends Base {
     return this.#field?.every(column => column.length === this.#rows) ?? false;
   }
 
-  #hasWon() {
-    return false;
+  #isWin() {
+    return (
+      // vertical
+      this.#checkLine({ start: 0, fixed: true }, { start: -3 }) ||
+      // horizontal
+      this.#checkLine({ start: 0 }, { start: 0, fixed: true }) ||
+      this.#checkLine({ start: -1 }, { start: 0, fixed: true }) ||
+      this.#checkLine({ start: -2 }, { start: 0, fixed: true }) ||
+      this.#checkLine({ start: -3 }, { start: 0, fixed: true }) ||
+      // diagonal ascending
+      this.#checkLine({ start: 0 }, { start: 0 }) ||
+      this.#checkLine({ start: -1 }, { start: -1 }) ||
+      this.#checkLine({ start: -2 }, { start: -2 }) ||
+      this.#checkLine({ start: -3 }, { start: -3 }) ||
+      // diagonal descending
+      this.#checkLine({ start: 0 }, { start: 0, inverted: true }) ||
+      this.#checkLine({ start: -1 }, { start: 1, inverted: true }) ||
+      this.#checkLine({ start: -2 }, { start: 2, inverted: true }) ||
+      this.#checkLine({ start: -3 }, { start: 3, inverted: true })
+    );
+  }
+
+  #checkLine(_col: checkLineProps, _row: checkLineProps): boolean {
+    let col = this.#current.col + _col.start;
+    let row = this.#current.row + _row.start;
+    if (col < 0 || row < 0 || !this.#field) {
+      return false;
+    }
+    let count = 0;
+    for (let i = 0; i < CONNECT; i++) {
+      if (!this.#field[col]?.[row]) {
+        return false;
+      }
+      count += this.#field[col][row];
+      col += _col.fixed ? 0 : _col.inverted ? -1 : 1;
+      row += _row.fixed ? 0 : _row.inverted ? -1 : 1;
+    }
+    return count === this.currentPlayer * CONNECT;
   }
 }
 
